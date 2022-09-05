303384ebb4c6ac811417c0471770224016284ea0
rohit <rohitloke@gmail.com>
2022-09-05 22:50:27 +0200

    fixed issue with user creation and adding favouite recipes

--- rms-api/src/main/java/com/rms/assignment/api/dto/UserDto.java
+++ rms-api/src/main/java/com/rms/assignment/api/dto/UserDto.java
@@ -1,15 +1,21 @@
 package com.rms.assignment.api.dto;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class UserDto {
 
     private final String username;
-    private final List<RecipeDto> favouriteRecipes;
+    private final List<RecipeDto> favouriteRecipes = new ArrayList<>();
 
-    public UserDto(String username, List<RecipeDto> favouriteRecipes) {
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    public UserDto(@JsonProperty("username") String username,
+            @JsonProperty("favouriteRecipes") List<RecipeDto> favouriteRecipes) {
         this.username = username;
-        this.favouriteRecipes = favouriteRecipes;
+        this.favouriteRecipes.addAll(favouriteRecipes);
     }
 
     public String getUsername() {
--- rms-core/src/main/java/com/rms/assignment/core/service/impl/UserServiceImpl.java
+++ rms-core/src/main/java/com/rms/assignment/core/service/impl/UserServiceImpl.java
@@ -1,11 +1,16 @@
 package com.rms.assignment.core.service.impl;
 
+import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.rms.assignment.core.entity.Recipe;
 import com.rms.assignment.core.entity.User;
+import com.rms.assignment.core.repository.RecipeRepository;
 import com.rms.assignment.core.repository.UserRepository;
 import com.rms.assignment.core.service.UserService;
 
@@ -14,6 +19,8 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private RecipeRepository recipeRepository;
 
     @Override
     public Optional<User> getByUsername(String username) {
@@ -22,6 +29,11 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User create(User user) {
+        // USers don't create recipes they only favourite existing recipe
+        List<Recipe> recipes = user.getFavouriteRecipes().stream()
+                .map(r -> recipeRepository.getByName(r.getName()).orElse(null)).filter(Objects::nonNull)
+                .collect(Collectors.toList());
+        user.setFavouriteRecipes(recipes);
         return userRepository.save(user);
     }
 }
